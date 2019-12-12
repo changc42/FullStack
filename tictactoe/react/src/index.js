e81fb3f64d6c6d59406a1b520cc9e5924870691e
Caleb Chang <changc42@gmail.com>
2019-12-11 21:41:01 -0500

    changed "test" to "testV"

--- tictactoe/react/src/index.js
+++ tictactoe/react/src/index.js
@@ -1,15 +1,67 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Car from './Car.js';
+import './index.css';
 
-
-
-class Garage extends React.Component{
-	render(){
-		return(
-			<h1>Garage knows Car sayrrs "{<Car />}"</h1>
-		);
-	}
+class Square extends React.Component {
+  render() {
+    return (
+      <button className="square">
+        {/* TODO */}
+      </button>
+    );
+  }
 }
 
-ReactDOM.render(<Garage />, document.getElementById('root'));
+class Board extends React.Component {
+  renderSquare(i) {
+    return <Square />;
+  }
+
+  render() {
+    const status = 'Next player: X';
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
