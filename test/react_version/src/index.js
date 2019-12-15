fbaf94af631cf2c3021b66548514ad5079da6911
Caleb Chang <changc42@gmail.com>
2019-12-15 00:21:22 -0500

    update

---
+++ test/react_version/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+
+const element = <h1>Hello Wolrd</h1>;
+ReactDOM.render(element, document.getElementById("root"));
