6a0e0b49abe99ca43d27555fd9aebb54602ae03e
Caleb Chang <changc42@gmail.com>
2020-01-06 15:59:34 -0500

    completed dad_jokes

--- React/app_runner/src/DadJokesApp.js
+++ React/app_runner/src/DadJokesApp.js
@@ -2,31 +2,100 @@ import React, { Component } from 'react';
 import Joke from './Joke';
 import './DadJokesApp.css';
 import axios from 'axios';
+import uuid from 'uuid/v4';
 
 export default class DadJokesApp extends Component {
     static defaultProps = {
         initialLimit: 10,
     }
     state={
-        jokes: [],
+        jokeBars: JSON.parse(window.localStorage.getItem("jokes")) || [],
+        isLoaded: false,
     }
 
-async componentDidMount(){
-    while(this.props.initialLimit>this.state.jokes.length){
-        let response = await axios.get("https://icanhazdadjoke.com/", {headers: {Accept: "application/json"}});
-        this.setState({jokes: [...this.state.jokes, response.data.joke]});
-        this.setState({count: this.state.count+1});
+    upvote = this.upvote.bind(this);
+    upvote(id){
+        let index = this.state.jokeBars.map(e=>e.id).indexOf(id);
+        let temp = this.state.jokeBars;
+        temp[index].score += 1;
+        this.setState(st=> {return {jokeBars: temp}}, ()=>window.localStorage.setItem("jokes", JSON.stringify(this.state.jokeBars)));
     }
-}
+
+    downvote=this.downvote.bind(this);
+    downvote(id){
+        let index = this.state.jokeBars.map(e=>e.id).indexOf(id);
+        let temp = this.state.jokeBars;
+        temp[index].score -= 1;
+        this.setState({jokeBars: temp});
+    }
+
+    getNewJokes = this.getNewJokes.bind(this);
+    getNewJokes(){
+        this.setState(
+            st => {return {isLoaded: false, jokeBars: []}},
+            async ()=> this.getJokes() 
+        );
+    }
+
+    getJokes = this.getJokes.bind(this);
+    async getJokes(){
+        
+        this.setState({isLoaded:false});
+        while(this.props.initialLimit>this.state.jokeBars.length){
+            console.log("in loop");
+            let response = await axios.get("https://icanhazdadjoke.com/", {headers: {Accept: "application/json"}});
+            let isUnique = true;
+            this.state.jokeBars.forEach(e=>{
+                if(response.data.joke==this.state.jokeBars.joke) isUnique=false;
+            });
+            if(isUnique){
+                this.setState({
+                    jokeBars: [
+                        ...this.state.jokeBars,
+                        {joke: response.data.joke, score:0, id:uuid()}
+                    ]
+                });
+            }
+        }
+        this.setState({isLoaded: true});
+        window.localStorage.setItem("jokes", JSON.stringify(this.state.jokeBars));
+    }
+
+    async componentDidMount(){
+        this.getJokes();
+    }
 
 
   render() {
+      //sort jokes by score
+      let sortedJokes = [];
+      let{jokeBars} = this.state;
+      let temp = jokeBars.slice(0);
+      let idOfMax;
+
+      //one-by-one, move max element from temp to sortedJokes
+      while(temp.length>0){
+        let max=Number.MIN_SAFE_INTEGER;
+        for(let j=0; j<temp.length; j++){
+            if(temp[j].score>max){
+                max=temp[j].score;
+                idOfMax=temp[j].id;
+            }
+        }
+        temp.splice(temp.map(e=>e.id).indexOf(idOfMax),1);
+        sortedJokes.push(jokeBars[jokeBars.map(e=>e.id).indexOf(idOfMax)]);
+      }
     return (
       <div className="DadJokesApp">
-          <h1>Dad Jokes</h1>
-          <div>
-              {this.state.jokes.map(e=><Joke joke={e}/>)}
-          </div>
+        <h1>Dad Jokes</h1>
+        <button onClick={this.getNewJokes}>Get new jokes</button>
+        {this.state.isLoaded?(
+            <div>
+                {sortedJokes.map(e=><Joke downvote={this.downvote} upvote={this.upvote} id={e.id} joke={e.joke} score={e.score}/>)}
+            </div>
+            ):(
+                <i class="fas fa-spinner fa-10x rotate"></i>
+        )}
       </div>
     );
   }
