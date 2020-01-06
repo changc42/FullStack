9f670dc911fb7912a52118f50df02d0946a53bb9
Caleb Chang <changc42@gmail.com>
2020-01-06 15:03:00 -0500

    continued dad_jokes

---
+++ React/dad_jokes/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import DadJokesApp from './DadJokesApp';
+
+class App extends React.Component{
+  render(){
+    return (
+      <DadJokesApp />
+    );
+  }
+}
+
+export default App;
