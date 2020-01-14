5cd2eb6e91305c3c50e77ad2370ea514e5701e3d
Caleb Chang <changc42@gmail.com>
2020-01-14 17:09:27 -0500

    added project files

--- React/massive_color_project/src/App.js
+++ React/massive_color_project/src/App.js
@@ -2,11 +2,20 @@ import React from 'react';
 import Palette from './Palette';
 import seedColors from './seedColors';
 import {getPalette} from './ColorHelpers';
+import chroma from 'chroma-js';
 
 class App extends React.Component{
   render(){
+    let color = chroma("red").darken(-1).hex();
+    console.log(color);
     return (
-      <Palette palette={getPalette(seedColors[5])}/>
+      <div>
+        <div style={{backgroundColor: "red", height:"400px"}}>3</div>
+        <div style={{backgroundColor: `${color}`, height:"400px"}}>3</div>
+        <Palette palette={getPalette(seedColors[5])}/>
+        
+      </div>
+        
     );
   }
 }
---
+++ React/massive_color_project/srcTemplate/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class App extends React.Component{
+  render(){
+    return ("I am react app"
+    );
+  }
+}
+
+export default App;
